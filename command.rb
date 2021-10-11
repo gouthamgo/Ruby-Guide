156f3c1ca9c0bd087815d85b0a77942f7eb89f77
Goutham M <gouthamgo@yahoo.com>
2021-10-11 16:14:02 +1100

    Create command.rb

---
+++ command.rb
@@ -0,0 +1,43 @@
+=begin
+
+//Multipline comments start with begin and end
+
+# Ruby Variables
+
+myVar = 484
+
+
+
+# Put and print command
+# put and print commands can be used to display text in the console.
+
+print "Hello"
+puts " new line"
+
+
+#Ruby Object Methods
+In Ruby, methods are built-in abilities of objects. To access an object’s methods,
+you need to call it using a . and the method name.
+
+
+var = "General Assembly"
+ 
+# Method to get the length of a string
+print var.length # 10
+ 
+# Method to get the string reversed
+print var.reverse 
+ 
+# Method to conver all letters to uppercase
+print var.upcase 
+
+
+# Method to conver all letters to lowercase
+print var.downcase 
+
+
+
+
+
+
+=end
